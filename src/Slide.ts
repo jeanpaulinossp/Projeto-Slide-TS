0f23372b9e2e1e3bc2a3c812f1b7f96fc21235c1
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-03-07 18:01:00 -0300

    botoes do slde

--- src/Slide.ts
+++ src/Slide.ts
@@ -19,7 +19,7 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
 
   hide(el: Element) {
@@ -31,5 +31,32 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    const next = this.index + 1 < 4 ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
+
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    nextButton.addEventListener("pointerup", () => this.next());
+    prevButton.addEventListener("pointerup", () => this.prev());
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
